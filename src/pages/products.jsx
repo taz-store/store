8f7188cec7342ba91e35aeccac6cec43e1c4a72e
GT Fari <guideg6@gmail.com>
2020-08-10 16:15:03 +0200

    product item styled

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -16,6 +16,9 @@ const pageQuery = graphql`
           }
           price
           name
+          category {
+            name
+          }
         }
       }
   }
@@ -28,12 +31,27 @@ const products = () => {
     return (
         <Layout>
             {products.map((product) => (
-            <div className='relative px-6 py-10 mx-auto mt-16 cardContainer md:max-w-3xl' key={product.slug}>
-              <h1>{product.slug}</h1>
-              <Link to={`/products/${product.slug}`}>
-                {product.name}
-              </Link>
-            </div>
+            // <div className='relative px-6 py-10 mx-auto mt-16 cardContainer md:max-w-3xl' key={product.slug}>
+            //   <h1>{product.slug}</h1>
+            //   <Link to={`/products/${product.slug}`}>
+            //     {product.name}
+            //   </Link>
+            // </div>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2" key={product.slug}>
+        <Link to={product.slug} className="relative block h-48 overflow-hidden rounded">
+          <img alt="ecommerce" className="block object-cover object-center w-full h-full" src={product.image.url}/>
+        </Link>
+        <div className="mt-4">
+          <h3 className="mb-1 text-xs tracking-widest text-gray-500 title-font">{product.category.name}</h3>
+          <h2 className="text-lg font-medium text-gray-900 title-font">{product.name}</h2>
+          <p className='mt-1'>
+            {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            }).format(product.price)}
+        </p>
+        </div>
+      </div>
           ))}
         </Layout>
     )
@@ -45,3 +63,90 @@ export default products
     <Link key={slug} to={`/products/${slug}`}>
     {product.name}
     </Link> */}
+
+//     <section class="text-gray-700 body-font">
+//   <div class="container px-5 py-24 mx-auto">
+//     <div class="flex flex-wrap -m-4">
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+//           <p class="mt-1">$16.00</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/421x261">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+//           <p class="mt-1">$21.15</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/422x262">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+//           <p class="mt-1">$12.00</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/423x263">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+//           <p class="mt-1">$18.40</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/424x264">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
+//           <p class="mt-1">$16.00</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/425x265">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
+//           <p class="mt-1">$21.15</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/427x267">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">Neptune</h2>
+//           <p class="mt-1">$12.00</p>
+//         </div>
+//       </div>
+//       <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+//         <a class="block relative h-48 rounded overflow-hidden">
+//           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/428x268">
+//         </a>
+//         <div class="mt-4">
+//           <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+//           <h2 class="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
+//           <p class="mt-1">$18.40</p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </section>
--- src/templates/product-item.js
+++ src/templates/product-item.js
@@ -1,22 +1,61 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { graphql } from 'gatsby';
+import Layout from "../components/layout";
+
 
 const ProductPage = ({
   data: {
     gcms: { product },
   },
 }) => (
-  <React.Fragment>
-    <h1>{product.name}</h1>
-    <p>{product.description.text}</p>
-    <p>
-      {new Intl.NumberFormat('de-DE', {
-        style: 'currency',
-        currency: 'EUR',
-      }).format(product.price)}
-    </p>
-  </React.Fragment>
+  <Layout>
+  <section className="overflow-hidden text-gray-500 bg-gray-900 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap mx-auto lg:w-4/5">
+      <img alt="ecommerce" className="object-cover object-center w-full h-64 rounded lg:w-1/2 lg:h-auto" src={product.image.url}/>
+      <div className="w-full mt-6 lg:w-1/2 lg:pl-10 lg:py-6 lg:mt-0">
+        <h2 className="text-sm tracking-widest text-gray-600 title-font">{product.category}</h2>
+        <h1 className="mb-1 text-3xl font-medium text-white title-font">{product.name}</h1>
+        <p className="leading-relaxed">{product.description.text}</p>
+        <div className="flex items-center pb-5 mt-6 mb-5 border-b-2 border-gray-800">
+          <div className="flex">
+            <span className="mr-3">Color</span>
+            <button className="w-6 h-6 border-2 border-gray-800 rounded-full focus:outline-none"></button>
+            <button className="w-6 h-6 ml-1 bg-gray-700 border-2 border-gray-800 rounded-full focus:outline-none"></button>
+            <button className="w-6 h-6 ml-1 bg-indigo-500 border-2 border-gray-800 rounded-full focus:outline-none"></button>
+          </div>
+          <div className="flex items-center ml-6">
+            <span className="mr-3">Size</span>
+            <div className="relative">
+              <select className="py-2 pl-3 pr-10 text-white bg-gray-800 border border-gray-700 rounded appearance-none focus:outline-none focus:border-indigo-500">
+                <option>SM</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+              </select>
+              <span className="absolute top-0 right-0 flex items-center justify-center w-10 h-full text-center text-gray-600 pointer-events-none">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <span className="text-2xl font-medium text-white title-font">{new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(product.price)}
+          </span>
+          <button className="w-32 mx-auto btn">Button</button>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</Layout>
 );
 
 export const pageQuery = graphql`
@@ -27,13 +66,17 @@ export const pageQuery = graphql`
         description {
           text
         }
+        image {
+          url
+        }
         price
       }
     }
   }
-`;
+  `;
 
 export default ProductPage;
+  
 
 // import React from 'react'
 // import { graphql, } from 'gatsby';
@@ -55,12 +98,12 @@ export default ProductPage;
 //             image={post.frontmatter.image}
 //             article />
             
-//           <div className='prose text-gray-400 lg:prose-xl'> 
-//               <Img className='mb-10' image={post.frontmatter.image} alt={post.frontmatter.title} />
-//               <h1 className=''>{post.frontmatter.title}</h1>
+//           <div classNameName='prose text-gray-400 lg:prose-xl'> 
+//               <Img classNameName='mb-10' image={post.frontmatter.image} alt={post.frontmatter.title} />
+//               <h1 classNameName=''>{post.frontmatter.title}</h1>
 //               <h5>By {post.frontmatter.author}</h5>
 //               <h5>On {post.frontmatter.date}</h5>
-//               <div className='danger' dangerouslySetInnerHTML={{__html: post.html}}/>
+//               <div classNameName='danger' dangerouslySetInnerHTML={{__html: post.html}}/>
 //           </div>
 //         </Layout>
 //     )
