3325db4f5027be9fb1eef058d5ed72a024143350
GT Fari <guideg6@gmail.com>
2020-08-10 13:56:54 +0200

    graphcms connected

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,11 +34,19 @@ module.exports = {
         ],
       },
     },
+    // {
+    //   resolve: `gatsby-source-contentful`,
+    //   options: {
+    //     spaceId: process.env.CONTENTFUL_SPACE_ID,
+    //     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    //   },
+    // },
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: `gatsby-source-graphql`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        typeName: `GraphCMS`,
+        fieldName: `gcms`,
+        url: process.env.GRAPHCMS_URL,
       },
     },
     {
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,13 +1,47 @@
+import { graphql, useStaticQuery, Link  } from 'gatsby';
 import React from 'react'
 import Layout from "../components/layout";
 
+const pageQuery = graphql`
+  {
+    gcms {
+        products {
+          description {
+            text
+            html
+          }
+          slug
+          image {
+            url
+          }
+          price
+          name
+        }
+      }
+  }
+`;
+
 
 const products = () => {
+    const { gcms: { products }, } = useStaticQuery(pageQuery);
+    // console.log(products)
     return (
         <Layout>
-            
+            {products.map((product) => (
+            <div className='relative px-6 py-10 mx-auto mt-16 cardContainer md:max-w-3xl' key={product.slug}>
+              <h1>{product.slug}</h1>
+              <Link to={`/products/${product.slug}`}>
+                {product.name}
+              </Link>
+            </div>
+          ))}
         </Layout>
     )
 }
 
 export default products
+
+{/* {products}.map(({ product }) => (
+    <Link key={slug} to={`/products/${slug}`}>
+    {product.name}
+    </Link> */}
