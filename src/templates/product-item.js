90a0774d87281f9c569b82fe38180f3cb7c50c40
GT Fari <guideg6@gmail.com>
2020-08-19 19:49:11 +0200

    sizes select component wip

--- src/templates/product-item.js
+++ src/templates/product-item.js
@@ -11,6 +11,20 @@ console.log(product.image.node.childImageSharp.fluid)
 const sizes = (product.sizes)
 console.log(sizes)
   
+function SizeInput() {
+  if(sizes.length > 0) {
+      return (
+      <select className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" name="size">
+        {sizes.map((size, index) => (
+              <option key={index} value={size}>{size}</option>
+              ))}
+      </select>
+      )
+  } else {
+      return <span className='hidden'></span>
+  }
+}
+
   return(
   <Layout>
   <section className="overflow-hidden text-gray-500 shadow-xl body-font">
@@ -23,31 +37,6 @@ console.log(sizes)
         <h2 className="text-sm tracking-widest text-gray-600 title-font">{product.category.name}</h2>
         <h1 className="mb-1 text-3xl font-medium text-white title-font">{product.name}</h1>
         <p className="leading-relaxed">{product.description.text}</p>
-        
-        {/* <div className="flex items-center pb-5 mt-6 mb-5 border-b-2 border-gray-800">
-          <div className="flex">
-            <span className="mr-3">Color</span>
-            <button className="w-6 h-6 border-2 border-gray-800 rounded-full focus:outline-none"></button>
-            <button className="w-6 h-6 ml-1 bg-gray-700 border-2 border-gray-800 rounded-full focus:outline-none"></button>
-            <button className="w-6 h-6 ml-1 bg-indigo-500 border-2 border-gray-800 rounded-full focus:outline-none"></button>
-          </div>
-          <div className="flex items-center ml-6">
-            <span className="mr-3">Size</span>
-            <div className="relative">
-              <select className="py-2 pl-3 pr-10 text-white bg-gray-800 border border-gray-700 rounded appearance-none focus:outline-none focus:border-indigo-500">
-                <option>SM</option>
-                <option>M</option>
-                <option>L</option>
-                <option>XL</option>
-              </select>
-              <span className="absolute top-0 right-0 flex items-center justify-center w-10 h-full text-center text-gray-600 pointer-events-none">
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
-                  <path d="M6 9l6 6 6-6"></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        </div> */}
         <div className="flex">
           <span className="text-2xl font-medium text-white title-font">{new Intl.NumberFormat('en-US', {
               style: 'currency',
@@ -59,6 +48,7 @@ console.log(sizes)
     </div>
   <div className="flex mx-auto lg:w-4/5">
   </div>
+
   <form name="ProductItem" data-netlify="true" method='POST' data-netlify-honeypot="bot-field"  className="flex flex-col w-full mt-8 md:ml-auto md:py-8 md:mt-0">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="ProductItem" />
@@ -67,14 +57,10 @@ console.log(sizes)
       <p className="mb-5 leading-relaxed text-gray-600">Please enter your details and we&#39;ll get back to you as soon as possible.</p>
       <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Name" type="text" name="name"/>
       <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Email" type="email" name="email"/>
-      <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Quantity" type="number" name="quantity"/>
+      <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" min="1" placeholder="Quantity" type="number" name="quantity"/>
       <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Address 1" type="text" name="addressOne"/>
       <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Address 2" type="text" name="addressTwo"/>
-      <select className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" name="size">
-          {sizes.map((size, index) => (
-                <option key={index} value={size}>{size}</option>
-                ))}
-      </select>
+      <SizeInput/>
       <button className="mx-auto btn" type='submit'>Submizzle</button>
       <p className="mt-3 text-xs text-gray-500">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
     </form>
