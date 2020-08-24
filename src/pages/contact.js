607e94d0b2dc7f8861bb07e70c78d24563eb4848
GT Fari <guideg6@gmail.com>
2020-08-25 00:30:21 +0200

    styling touch up

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const contact = () => {
                     <form name="contact" data-netlify="true" method='POST' data-netlify-honeypot="bot-field" className= "relative z-10 flex flex-col w-full p-8 mt-10 rounded-lg md:ml-auto md:mt-0">
                         <input type="hidden" name="bot-field" />
                         <input type="hidden" name="form-name" value="contact" />
-                        <h2 className="mb-1 text-lg font-medium text-white title-font">Say Hello</h2>
+                        <h2 className="mb-4 text-xl font-medium text-center text-white title-font">Say Hello</h2>
                         <p className="mb-5 leading-relaxed text-custom-offWhite">We&apos;re here to discuss any questions you may have, how can we help?</p>
                         <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Name" type="text" name='name'/>
                         <input className="px-4 py-2 mb-4 text-base text-white bg-gray-800 border border-gray-700 rounded focus:outline-none focus:border-indigo-500" placeholder="Email" type="email" name='email'/>
